5d1884d342f9cf6eaace21fd1cd21f6ee8934257
Jaro Hokkanen <jaro.hokkanen@csc.fi>
2023-09-19 11:23:38 +0300

    Make weights to be always nonzero

--- grid.cpp
+++ grid.cpp
@@ -505,7 +505,7 @@ void balanceLoad(dccrg::Dccrg<SpatialCell,dccrg::Cartesian_Geometry>& mpiGrid, S
 //      if (P::propagateVlasovAcceleration) 
 //        mpiGrid.set_cell_weight(cells[i], mpiGrid[cells[i]]->parameters[CellParams::LBWEIGHTCOUNTER]);
 //      else
-        mpiGrid.set_cell_weight(cells[i], mpiGrid[cells[i]]->get_number_of_all_velocity_blocks());
+        mpiGrid.set_cell_weight(cells[i], (Real) (1 + mpiGrid[cells[i]]->get_number_of_all_velocity_blocks()));
       //reset counter
       //mpiGrid[cells[i]]->parameters[CellParams::LBWEIGHTCOUNTER] = 0.0;
    }
